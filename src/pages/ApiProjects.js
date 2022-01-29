08ac5a2607d74567802d53ca60bce0007768c9d2
vuoso-l <vuosolucas@gmail.com>
2022-01-29 17:11:18 -0300

    Added anchor url prop

--- src/pages/ApiProjects.js
+++ src/pages/ApiProjects.js
@@ -8,8 +8,16 @@ const ApiProject = () => {
   return (
     <HomeStyle>
       <Header />
-      <CardComponent name="Web Manteca & Harina" urlImg="https://res.cloudinary.com/lupevu/image/upload/v1643460719/logo_m_h_rdgobd.jpg" />
-      <CardComponent name="GitHub de la API Rest de M & H" urlImg="https://res.cloudinary.com/lupevu/image/upload/v1643460719/logo_github_uoyzl1.png" />
+      <CardComponent
+        name="Web Manteca & Harina"
+        urlImg="https://res.cloudinary.com/lupevu/image/upload/v1643460719/logo_m_h_rdgobd.jpg"
+        href="https://vuoso-l.github.io/client-manteca-y-harina/"
+      />
+      <CardComponent
+        name="GitHub de la API Rest de M & H"
+        urlImg="https://res.cloudinary.com/lupevu/image/upload/v1643460719/logo_github_uoyzl1.png"
+        href="https://github.com/vuoso-l/api-manteca-y-harina/"
+      />
       <Footer />
     </HomeStyle>
   );
