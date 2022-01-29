c0c6d2abab6832bd023c4f9cac77b4d3b2302d99
vuoso-l <vuosolucas@gmail.com>
2022-01-29 17:12:27 -0300

    Added colors

--- src/styleAux/theme.js
+++ src/styleAux/theme.js
@@ -1,14 +1,25 @@
 export const color = {
-    bgPrimary: "rgb(61, 61, 240)",
-    bgSecondary: "#3f87a6",
-    addBtn: "#0000ffb0",
-}
+  titleHeader: "white",
+  titleCard: "black",
+  bgPrimary: "rgb(61, 61, 240)",
+  bgSecondary: "#3f87a6",
+  button: "black",
+  buttonHover: "white",
+  aFooter: "white",
+};
 
 export const displayFlex = (display, direction, justifyContent, alignItem) => {
-    return `
+  return `
     display: ${display};
     flex-direction: ${direction};
     justify-content: ${justifyContent};
     align-items: ${alignItem};
-    `    
-}
+    `;
+};
+
+export const backgroundHeader = (url) => {
+  return `
+    background: ${color.bgPrimary} url(${url}) no-repeat;
+    background-size: cover;    
+    `;
+};
