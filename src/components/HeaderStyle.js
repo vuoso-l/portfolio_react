79367be0a3f72024208fb7a7ebe1b37a76c5e8e6
vuoso-l <vuosolucas@gmail.com>
2022-01-30 19:23:40 -0300

    Updated styles

--- src/components/HeaderStyle.js
+++ src/components/HeaderStyle.js
@@ -3,7 +3,7 @@ import { device } from "../styleAux/deviceSize";
 import { displayFlex, backgroundHeader } from "../styleAux/theme";
 
 export const HeaderStyle = styled.header`
-  ${displayFlex("flex", "column", "space-evenly", "center")};
+  ${displayFlex("flex", "column", "center", "center")};
   width: 100%;
   height: 150px;
   ${backgroundHeader("https://res.cloudinary.com/lupevu/image/upload/v1643460719/portada_linkedin2_hvcxj4.jpg")}
