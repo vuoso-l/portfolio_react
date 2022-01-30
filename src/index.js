5ac53a7dc2c36e206e169fade0502422eb0b8fec
vuoso-l <vuosolucas@gmail.com>
2022-01-29 21:09:41 -0300

    Added Router from react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,7 +9,9 @@ import { GlobalStyle } from './components/GlobalStyle';
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
