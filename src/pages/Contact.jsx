f3403baee6f931dd042bfacf7956b3fe2dd07671
vuoso-l <vuosolucas@gmail.com>
2022-04-07 06:16:53 -0300

    feat: added basic styles

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import CardTitle from "../components/CardTitle";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import ThemeContext from "../context/ThemeContext";
+import { ContainerStyle } from "../stylesComponents/ContainerStyle";
 
 const Contact = () => {
+  const { theme } = useContext(ThemeContext);
   return (
-    <>
+    <ContainerStyle theme={theme}>
       <Header />
+      <CardTitle title="Proyectos" nav="/" />
       <h2>Contact</h2>
       <Footer />
-    </>
+    </ContainerStyle>
   );
 };
 
